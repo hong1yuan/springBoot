065df9a481dd0410127981ef2e232cac52673590
chenpeng <357370303@qq.com>
2018-07-17 11:19:36 +0800

    修改mapper扫描 和增加sql

--- src/main/java/com/spring/springboot/springBootApplication.java
+++ src/main/java/com/spring/springboot/springBootApplication.java
@@ -1,5 +1,6 @@
 package com.spring.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 //@EnableAutoConfiguration
 //@ComponentScan(basePackages={"com.spring.springboot"})
+//扫描dao层 有@MapperScan(注解则不需要在mapper接口中写@Mapper注解（启动类中写@MapperScan和mapper接口中写@Mapper注解存在一个就行）
+@MapperScan(basePackages= {"com.spring.springboot.mapper"})
 @EnableScheduling	//增加支持定时任务的注解
 @EnableAsync		//多线程
 public class springBootApplication {
